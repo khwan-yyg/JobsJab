ebf19eab39237397a38fc2a0ad3c5583a7e0204a
khwankamon <khwan_yyg@outlook.com>
2025-01-19 12:50:53 +0700

    feat(recruiterlogin): add login signup ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,22 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
+import RecruiterLogin from "./components/RecruiterLogin";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
 
 function App() {
+  const { showRecruiterLogin } = useContext(AppContext);
+
   return (
-    <>
+    <div>
+      {showRecruiterLogin && <RecruiterLogin />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
 import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 import { Link, useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const { openSignIn } = useClerk();
   const { user } = useUser();
 
   const navigate = useNavigate();
+
+  const { setShowRecruiterLogin } = useContext(AppContext);
 
   return (
     <div className="shadow py-4">
@@ -28,7 +31,12 @@ const Navbar = () => {
           </div>
         ) : (
           <div className="flex gap-4 max-sm:text-sm">
-            <button className="text-gray-600">Recruiter Login</button>
+            <button
+              onClick={(e) => setShowRecruiterLogin(true)}
+              className="text-gray-600"
+            >
+              Recruiter Login
+            </button>
             <button
               onClick={(e) => openSignIn()}
               className="bg-blue-600 text-white px-6 sm:px-9 py-2 rounded-full"
